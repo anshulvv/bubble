14cb730d4cffea278c374be22014a1c193b50c81
Anshul Verma <thisisanshulverma@gmail.com>
2024-06-01 17:13:20 +0900

    added bfs to pop all bubbles of same color near the area

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Element
 import Element.Background
 import Element.Border
 import Element.Events
+import Set
 
 
 type Color
@@ -12,7 +13,7 @@ type Color
     | Red
     | Blue
     | Yellow
-    | Brown
+    | Black
 
 
 
@@ -69,15 +70,15 @@ bubbleGrid matrix =
 
 
 bubbleGridRow x row =
-    Element.row [ Element.spacing 3 ] (List.indexedMap (\ind bub -> bubble bub x ind) row)
+    Element.row [ Element.spacing 3 ] (List.indexedMap (\ind bubble -> viewBubble bubble x ind) row)
 
 
-bubble bub x y =
+viewBubble bubble x y =
     Element.el
-        [ Element.Background.color (getRgbColor bub |> (\( r, g, b ) -> Element.rgb255 r g b))
+        [ Element.Background.color (getRgbColor bubble |> (\( r, g, b ) -> Element.rgb255 r g b))
         , Element.width (Element.px bubbleDiameter)
         , Element.height (Element.px bubbleDiameter)
-        , Element.Border.rounded 10
+        , Element.Border.rounded 20
         , Element.Events.onClick (ClickedBubble x y)
         ]
         Element.none
@@ -89,13 +90,13 @@ bubbleDiameter =
 
 
 getRgbColor : Bubble -> ( Int, Int, Int )
-getRgbColor bub =
-    case bub.state of
+getRgbColor bubble =
+    case bubble.state of
         Popped ->
             ( 255, 255, 255 )
 
         Unpopped ->
-            case bub.color of
+            case bubble.color of
                 Red ->
                     ( 255, 0, 0 )
 
@@ -108,7 +109,7 @@ getRgbColor bub =
                 Yellow ->
                     ( 0, 125, 125 )
 
-                Brown ->
+                Black ->
                     ( 0, 0, 0 )
 
 
@@ -117,27 +118,118 @@ update msg model =
     case msg of
         ClickedBubble x y ->
             let
-                newMatrix : Matrix Bubble
+                targetBubble : Maybe Bubble
+                targetBubble =
+                    getBubble x y model.matrix
+
                 newMatrix =
-                    List.indexedMap
-                        (\indexRow row ->
-                            if indexRow == x then
-                                List.indexedMap
-                                    (\indexCol bub ->
-                                        if indexCol == y then
-                                            { bub | state = Popped }
+                    case targetBubble of
+                        Just bubble ->
+                            bfsBubble x y model.matrix bubble.color
 
-                                        else
-                                            bub
-                                    )
-                                    row
-
-                            else
-                                row
-                        )
-                        model.matrix
+                        Nothing ->
+                            model.matrix
             in
             { model | matrix = newMatrix }
+
+
+getBubble : Int -> Int -> Matrix Bubble -> Maybe Bubble
+getBubble x y matrix =
+    List.drop x matrix
+        |> List.head
+        |> Maybe.andThen (List.drop y >> List.head)
+
+
+bfsBubble x y matrix color =
+    let
+        queue =
+            [ ( x, y ) ]
+
+        visited =
+            Set.empty
+    in
+    bfsHelper queue visited matrix color
+
+
+bfsHelper queue visited matrix color =
+    case queue of
+        [] ->
+            matrix
+
+        ( row, col ) :: rest ->
+            if Set.member ( row, col ) visited then
+                bfsHelper rest visited matrix color
+
+            else
+                let
+                    bubble =
+                        getBubble row col matrix
+
+                    updatedVisited =
+                        Set.insert ( row, col ) visited
+
+                    newQueue =
+                        rest ++ List.filter (\( r, c ) -> isValidBubble r c matrix && isSameColor r c matrix color) (neighbors row col)
+
+                    newMatrix =
+                        case bubble of
+                            Just _ ->
+                                changeBubbleState row col matrix
+
+                            Nothing ->
+                                matrix
+                in
+                bfsHelper newQueue updatedVisited newMatrix color
+
+
+isValidBubble : Int -> Int -> Matrix Bubble -> Bool
+isValidBubble r c matrix =
+    r < List.length matrix && c < (List.length <| Maybe.withDefault [] (List.head matrix))
+
+
+isSameColor : Int -> Int -> Matrix Bubble -> Color -> Bool
+isSameColor r c matrix color =
+    let
+        bubble : Maybe Bubble
+        bubble =
+            matrix |> List.drop r |> List.head |> Maybe.andThen (List.drop c >> List.head)
+    in
+    case bubble of
+        Just bub ->
+            bub.color == color
+
+        Nothing ->
+            False
+
+
+neighbors : Int -> Int -> List ( Int, Int )
+neighbors r c =
+    [ ( r + 1, c )
+    , ( r - 1, c )
+    , ( r, c + 1 )
+    , ( r, c - 1 )
+    ]
+
+
+changeBubbleState : Int -> Int -> Matrix Bubble -> Matrix Bubble
+changeBubbleState r c matrix =
+    List.indexedMap
+        (\ir row ->
+            if ir == r then
+                List.indexedMap
+                    (\ic bubble ->
+                        if ic == c then
+                            { bubble | state = Popped }
+
+                        else
+                            bubble
+                    )
+                    row
+
+            else
+                row
+        )
+        matrix
 
 
 
@@ -157,11 +249,11 @@ update msg model =
 
 initialMatrix : Matrix Bubble
 initialMatrix =
-    [ [ { state = Unpopped, color = Green }, { state = Unpopped, color = Red }, { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow }, { state = Unpopped, color = Brown } ]
-    , [ { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow }, { state = Unpopped, color = Brown }, { state = Unpopped, color = Green }, { state = Unpopped, color = Red } ]
-    , [ { state = Unpopped, color = Yellow }, { state = Unpopped, color = Brown }, { state = Unpopped, color = Green }, { state = Unpopped, color = Red }, { state = Unpopped, color = Blue } ]
-    , [ { state = Unpopped, color = Brown }, { state = Unpopped, color = Green }, { state = Unpopped, color = Red }, { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow } ]
-    , [ { state = Unpopped, color = Red }, { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow }, { state = Unpopped, color = Brown }, { state = Unpopped, color = Green } ]
+    [ [ { state = Unpopped, color = Green }, { state = Unpopped, color = Red }, { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow }, { state = Unpopped, color = Black } ]
+    , [ { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow }, { state = Unpopped, color = Green }, { state = Unpopped, color = Green }, { state = Unpopped, color = Red } ]
+    , [ { state = Unpopped, color = Yellow }, { state = Unpopped, color = Green }, { state = Unpopped, color = Green }, { state = Unpopped, color = Red }, { state = Unpopped, color = Blue } ]
+    , [ { state = Unpopped, color = Black }, { state = Unpopped, color = Green }, { state = Unpopped, color = Red }, { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow } ]
+    , [ { state = Unpopped, color = Red }, { state = Unpopped, color = Blue }, { state = Unpopped, color = Yellow }, { state = Unpopped, color = Black }, { state = Unpopped, color = Green } ]
     ]
 
 
